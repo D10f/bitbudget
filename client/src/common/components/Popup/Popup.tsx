3588682c80043acb8b1fb585b53e434b0d4a9aaf
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-01-07 23:35:38 +0100

    refactored popup code for better usability and readability

--- client/src/common/components/Popup/Popup.tsx
+++ client/src/common/components/Popup/Popup.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import useMediaQuery from "@hooks/useMediaQuery";
+import React, { useLayoutEffect, useRef } from "react";
 import { StyledPopup } from './Popup.styled';
+import { breakpoints } from '@constants';
+import { useClickOutside } from "@hooks/useClickOutside";
 
 interface IPopupProps {
   children: React.ReactChild[];
-  align?: string;
+  align?: "bottom" | "top" | "right" | "left";
+  ref?: React.RefObject<HTMLElement>
+  onClickOutside?: () => void;
 }
 
-const slideInMotion = {
+const slideInLeft = {
   initial: {
     opacity: 0,
     x: 50,
@@ -28,20 +33,56 @@ const slideInMotion = {
   },
 };
 
-const Popup = React.forwardRef(({ children, align }: IPopupProps, ref) => {
+const slideInRight = {
+  initial: {
+    opacity: 0,
+    x: 0,
+  },
+  visible: {
+    opacity: 1,
+    x: -50,
+    transition: {
+      type: "spring",
+      duration: 0.3,
+    },
+  },
+  exit: {
+    opacity: 0,
+    x: -100,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
+
+const Popup = ({ children, align, onClickOutside }: IPopupProps) => {
+
+  const popupRef = useRef<HTMLElement>(null);
+  const isMobile = useMediaQuery(`(max-width: ${breakpoints.md})`);
+  
+  useClickOutside(popupRef, () => {
+    if (onClickOutside) {
+      onClickOutside();
+    }
+  });
+  
+  useLayoutEffect(() => {
+    console.log(popupRef.current);
+  }, [popupRef]);
+
   return (
     <StyledPopup
-      variants={slideInMotion}
+      variants={isMobile ? slideInRight : slideInLeft}
       initial="initial"
       animate="visible"
       exit="exit"
       key="1"
-      ref={ref}
+      ref={popupRef}
       align={align}
     >
       {children}
     </StyledPopup>
   );
-});
+};
 
 export default Popup;
