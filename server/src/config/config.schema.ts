a16c6ef7f7e4262085b24b4d809ac6a655e58138
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-01-19 19:25:51 +0100

    make a test case fail on purpose to check Jenkins pipeline

--- server/src/config/config.schema.ts
+++ server/src/config/config.schema.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
-  PORT: Joi.number().default(3000),
+  PORT: Joi.number().default(80),
   DOMAIN: Joi.string().default('http://localhost'), // used in production to restrict CORS origin
   MONGO_URI: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
