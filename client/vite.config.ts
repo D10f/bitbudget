ee032672194abb27f1b51a4a88b86e4a10d473c7
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-08-29 21:45:11 +0200

    Updated import aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,11 @@ import path from 'path';
 export default defineConfig({
     resolve: {
         alias: {
+            '@app': path.resolve(__dirname, 'src/app'),
             '@assets': path.resolve(__dirname, 'src/assets'),
             '@components': path.resolve(__dirname, 'src/components'),
             '@features': path.resolve(__dirname, 'src/features'),
+            '@routes': path.resolve(__dirname, 'src/routes'),
         },
     },
     plugins: [react()],
