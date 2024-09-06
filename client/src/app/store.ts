f42b096c5e5b5d6af3195af9f6cc040437c96f11
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-09-06 15:22:27 +0200

    Fix accidental duplication of store definition. Updated to use user slice.

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '@features/auth/authSlice';
+import userReducer from '@features/user/userSlice';
 import { baseApi } from '@app/api/base';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 export const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
         auth: authReducer,
+        user: userReducer,
     },
     middleware(getDefaultMiddleware) {
         return getDefaultMiddleware().concat(baseApi.middleware);
@@ -15,3 +18,6 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
