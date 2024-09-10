d503937e6b4c10b8be3e608baae67b66dd35c71d
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-09-11 00:18:16 +0200

    Refactor to reduce the amount of logic handled in the defined endpoints

--- client/src/app/api/auth.ts
+++ client/src/app/api/auth.ts
@@ -1,78 +1,26 @@
 import { baseApi } from '@app/api/base';
-import { userApi } from '@app/api/user';
-import { setToken } from '@features/auth/authSlice';
 import { HTTP_METHOD } from '../../types/http';
-import {
-    generateMasterKey,
-    generateVaultKey,
-    serializeKey,
-} from '../../services/keys';
-import { addKey, resetKeys, setUserData } from '@features/user/userSlice';
-import type { AuthResponse, Credentials } from '@features/auth/types';
-import { RootState } from '@app/store';
+import type {
+    AuthSignupResponse,
+    AuthLoginResponse,
+    Credentials,
+} from '@features/auth/types';
 
 export const authApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
-        signup: builder.mutation<AuthResponse, Credentials>({
-            queryFn: async (
-                { name, email, password },
-                { dispatch, getState },
-                _extra,
-                baseQuery,
-            ) => {
-                try {
-                    const { key, hash } = await generateMasterKey(
-                        name,
-                        password,
-                    );
-
-                    const res = await baseQuery({
-                        url: '/auth/signup',
-                        method: HTTP_METHOD.POST,
-                        body: {
-                            name,
-                            email,
-                            password: hash,
-                        },
-                    });
-
-                    // Update access token
-                    dispatch(setToken((res.data as AuthResponse).accessToken));
-
-                    // Update local state
-                    const vaultKey = await generateVaultKey();
-                    dispatch(addKey(await serializeKey(vaultKey)));
-                    dispatch(addKey(await serializeKey(key)));
-                    dispatch(setUserData({ name, email }));
-
-                    // Update remote state. This creates the initial user
-                    // preferences on the server, along with the
-                    // encrypted vault key
-                    await dispatch(
-                        userApi.endpoints.updateUser.initiate({
-                            email,
-                            prefs: (getState() as RootState).user.prefs,
-                            vaultKey: await serializeKey(vaultKey),
-                        }),
-                    );
-
-                    return { data: res.data as AuthResponse };
-                } catch (error) {
-                    dispatch(resetKeys());
-                    return { error };
-                }
-            },
+        signup: builder.mutation<AuthSignupResponse, Credentials>({
+            query: (credentials) => ({
+                url: '/auth/signup',
+                method: HTTP_METHOD.POST,
+                body: credentials,
+            }),
         }),
-        login: builder.mutation<AuthResponse, Credentials>({
-            query: (credentials: Credentials) => ({
+        login: builder.mutation<AuthLoginResponse, Credentials>({
+            query: (credentials) => ({
                 url: '/auth/login',
                 method: HTTP_METHOD.POST,
                 body: credentials,
             }),
-            transformResponse: (response: AuthResponse) => {
-                // decrypt user data before returning
-                return response;
-            },
         }),
         logout: builder.mutation({
             query: () => ({
